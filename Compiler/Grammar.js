a6ac2ae4ac36134b34d16f9a5ac0ad5e329d8dab
EricMcCaw <40258078+EricMcCaw@users.noreply.github.com>
2020-01-28 00:41:11 -0500

    finished lab

--- Compiler/Grammar.js
+++ Compiler/Grammar.js
@@ -2,13 +2,21 @@
 exports.__esModule = true;
 var Grammar = /** @class */ (function () {
     function Grammar(input) {
+        var _this = this;
         this.grammarSet = new Set();
         this.terminals = new Array();
+        this.nonTerminals = new Array();
+        this.nodeList = new Array();
         var newInput = input.split("\n");
         var rex = /(.*?\s)(->\s)(.*)/g;
         this.terminals.push(new Terminal("WHITESPACE", /\s+/gy));
+        var swapped = false;
         for (var i = 0; i < newInput.length - 1; i++) {
             rex.lastIndex = 0;
+            if (newInput[i].length == 0) {
+                swapped = true;
+                continue;
+            }
             var output = rex.exec(newInput[i]);
             var name_1 = void 0;
             var newRex = void 0;
@@ -23,15 +31,71 @@ var Grammar = /** @class */ (function () {
                 throw new Error("conflicting regex");
             }
             else {
-                try {
-                    var testRex = new RegExp(newRex, "gy");
-                    this.grammarSet.add(name_1);
-                    this.terminals.push(new Terminal(name_1.slice(0, -1), testRex));
+                if (!swapped) {
+                    try {
+                        var testRex = new RegExp(newRex, "gy");
+                        this.grammarSet.add(name_1.replace(/\s/g, ''));
+                        this.terminals.push(new Terminal(name_1.replace(/\s/g, ''), testRex));
+                    }
+                    catch (_a) {
+                        throw new Error("this regex sucks");
+                    }
                 }
-                catch (_a) {
-                    throw new Error("this regex sucks");
+                else {
+                    try {
+                        this.grammarSet.add(name_1.replace(/\s/g, ''));
+                        this.nonTerminals.push(new NonTerminal(name_1.replace(/\s/g, ''), newRex));
+                    }
+                    catch (_b) {
+                        throw new Error("this regex sucks");
+                    }
                 }
             }
+        }
+        this.grammarSet.forEach(function (name) { return _this.nodeList.push(new NodeType(name)); });
+        var _loop_1 = function (i) {
+            var currentNode;
+            this_1.nodeList.forEach(function (elem) {
+                if (elem.label == _this.nonTerminals[i].leftSide) {
+                    currentNode = elem;
+                }
+            });
+            if (currentNode == null) {
+                throw new Error("we have a problem jimbo");
+            }
+            var TempArray = this_1.nonTerminals[i].rightSide.split("|");
+            var tempstring = void 0;
+            tempstring = TempArray.join('');
+            TempArray = tempstring.split(" ");
+            TempArray.forEach(function (elem) {
+                var found = false;
+                if (elem != '') {
+                    _this.nodeList.forEach(function (node) {
+                        if (node.label == elem) {
+                            if (!currentNode.nodeList.includes(node)) {
+                                currentNode.nodeList.push(node);
+                            }
+                            found = true;
+                        }
+                    });
+                    if (!found) {
+                        throw new Error("honestly its not good that we even made it here");
+                    }
+                }
+            });
+        };
+        var this_1 = this;
+        for (var i = 0; i < this.nonTerminals.length; i++) {
+            _loop_1(i);
+        }
+        var start = findNode(this.nonTerminals[0].leftSide, this.nodeList);
+        if (start.label == "NULL") {
+            throw new Error("Should have found this bug before now");
+        }
+        var visitedSet = new Set();
+        dfs(start, visitedSet);
+        if (visitedSet.size != this.nodeList.length) {
+            throw new Error("not everything was visited");
         }
     }
     return Grammar;
@@ -44,3 +108,39 @@ var Terminal = /** @class */ (function () {
     }
     return Terminal;
 }());
+var NonTerminal = /** @class */ (function () {
+    function NonTerminal(left, right) {
+        this.leftSide = left;
+        this.rightSide = right;
+    }
+    return NonTerminal;
+}());
+var NodeType = /** @class */ (function () {
+    function NodeType(label) {
+        this.label = label;
+        this.nodeList = [];
+    }
+    return NodeType;
+}());
+function dfs(currNode, visited) {
+    visited.add(currNode.label);
+    currNode.nodeList.forEach(function (nextNode) {
+        if (!visited.has(nextNode.label))
+            dfs(nextNode, visited);
+    });
+}
+function findNode(name, nodeList) {
+    var found = false;
+    var tempNode;
+    nodeList.forEach(function (node) {
+        if (node.label == name) {
+            found = true;
+            tempNode = node;
+        }
+    });
+    if (!found) {
+        return new NodeType("NULL");
+    }
+    else
+        return tempNode;
+}
--- Compiler/Grammar.ts
+++ Compiler/Grammar.ts
@@ -1,3 +1,4 @@
+import { isNullOrUndefined } from "util";
 
 
 
@@ -5,21 +6,27 @@ export class Grammar
 {
     grammarSet: Set<string> = new Set();
     terminals: Array<Terminal> = new Array();
+    nonTerminals: Array<NonTerminal> = new Array();
+    nodeList: Array<NodeType> = new Array();
+    startingNode: string;
     constructor(input: string) {
         let newInput = input.split("\n");
         var rex = /(.*?\s)(->\s)(.*)/g;
       
         this.terminals.push(new Terminal("WHITESPACE", /\s+/gy));
+        let swapped = false;
         for (let i = 0; i < newInput.length-1;i++) {
             rex.lastIndex = 0;
+            if (newInput[i].length == 0) {
+                swapped = true;
+                continue;
+            }
             let output = rex.exec(newInput[i]);
             let name: string;
             let newRex: string;
-            if (output) {
+            if (output ) {
                 name = output[1];
                 newRex = output[3];
-              
-
             }
             else {
                 throw new Error("invalid structure");
@@ -28,21 +35,85 @@ export class Grammar
                 throw new Error("conflicting regex");
             }
             else {
-                try {
-                    let testRex = new RegExp(newRex,"gy");
-                    this.grammarSet.add(name);
-                    this.terminals.push(new Terminal(name.slice(0,-1), testRex));
+                if (!swapped) {
+                    try {
+                        
+                        let testRex = new RegExp(newRex, "gy");
+                        this.grammarSet.add(name.replace(/\s/g, ''));
+                        this.terminals.push(new Terminal(name.replace(/\s/g, ''), testRex));
+                        
+                    }
+                    catch{
+                        throw new Error("this regex sucks");
+                    }
                 }
-                catch{
-                    throw new Error("this regex sucks");
+                else {
+                    try {
+                        
+                        this.grammarSet.add(name.replace(/\s/g, ''));
+                        this.nonTerminals.push(new NonTerminal(name.replace(/\s/g, ''), newRex));
+                        
+                    }
+                    catch{
+                        throw new Error("this regex sucks");
+                    }
                 }
+                
             }
+        }
+        this.grammarSet.forEach(name => this.nodeList.push(new NodeType(name)));
+        
+        for (let i = 0; i < this.nonTerminals.length; i++) {
+            let currentNode: NodeType;
+            this.nodeList.forEach(elem => {
+                if (elem.label == this.nonTerminals[i].leftSide ){
+                    currentNode = elem;
+                }
+            })
+            if (currentNode == null) {
+                throw new Error("we have a problem jimbo");
+            }
+
+            let TempArray = this.nonTerminals[i].rightSide.split("|");
+            let tempstring: string;
+            tempstring = TempArray.join('');
+            TempArray = tempstring.split(" ");
+            TempArray.forEach(elem => {
+                let found = false;
+                if (elem != '') {
+                    this.nodeList.forEach(node => {
+                        if (node.label == elem) {
+                            if (!currentNode.nodeList.includes(node)) {
+                                currentNode.nodeList.push(node);
+                            }
+                            
+                            found = true;
+                        }
+                        
+                    })
+                    if (!found) {
+                        throw new Error("honestly its not good that we even made it here");
+                    }
+                }
+            })
+        }
+       
+        let start:NodeType = findNode(this.nonTerminals[0].leftSide, this.nodeList);
+
+        if (start.label == "NULL") {
+            throw new Error("Should have found this bug before now");
+        }
+        
+        let visitedSet: Set<string> = new Set();
+        dfs(start, visitedSet);
+        if (visitedSet.size != this.nodeList.length) {
+            throw new Error("not everything was visited");
+            
         }
     }
 }
 
-class Terminal
-{
+class Terminal{
     sym: string;
     rex: RegExp;
 
@@ -52,4 +123,54 @@ class Terminal
 
     }
 
+}
+
+class NonTerminal {
+    leftSide: string;
+    rightSide: string;
+
+    constructor(left: string, right: string) {
+        this.leftSide = left;
+        this.rightSide = right;
+    }
+}
+
+class NodeType {
+
+    label: string;
+    nodeList: NodeType[];
+
+    constructor(label: string) {
+        this.label = label;
+        this.nodeList = [];
+
+    }
+}
+function dfs(currNode: NodeType, visited: Set<string>) {
+    
+    visited.add(currNode.label);
+    currNode.nodeList.forEach((nextNode: NodeType) => {
+        
+        if (!visited.has(nextNode.label))
+            dfs(nextNode, visited);
+    });
+}
+function findNode(name: string, nodeList: Array<NodeType>): NodeType {
+    let found = false;
+    let tempNode: NodeType;
+    nodeList.forEach(node => {
+
+        if (node.label == name) {
+            
+            found = true;
+            tempNode = node;
+        }
+
+    })
+    
+    if (!found) {
+        return new NodeType("NULL");
+    }
+    else
+        return tempNode;
 }
